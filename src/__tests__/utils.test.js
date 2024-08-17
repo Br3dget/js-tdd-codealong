c34ff3769ff9736e22b3d74c85919d99b1ebaefb
Bridget <bridget.njoki@student.moringaschool.com>
2024-08-17 23:16:28 +0300

    add

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,8 @@
-import { currentAgeForBirthYear } from "../utils";
+export function currentAgeForBirthYear(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
 
-describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
-  });
-});
+
+
+
+
